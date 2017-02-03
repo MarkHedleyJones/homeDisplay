f831e9c643414d1d868f70d9ba83a32bbe3ff2c4
Mark H. Jones <markjones112358@gmail.com>
2017-02-04 07:23:36 +1300

    Perform full refresh at midnight

--- http/js/main.js
+++ http/js/main.js
@@ -74,6 +74,23 @@ function renderDate() {
 	setTimeout(renderDate, 1000);
 }
 
+function refreshAt(hours, minutes, seconds) {
+    var now = new Date();
+    var then = new Date();
+
+    if(now.getHours() > hours ||
+       (now.getHours() == hours && now.getMinutes() > minutes) ||
+        now.getHours() == hours && now.getMinutes() == minutes && now.getSeconds() >= seconds) {
+        then.setDate(now.getDate() + 1);
+    }
+    then.setHours(hours);
+    then.setMinutes(minutes);
+    then.setSeconds(seconds);
+
+    var timeout = (then.getTime() - now.getTime());
+    setTimeout(function() { window.location.reload(true); }, timeout);
+}
+
 function renderCalendar() {
 	$('#caleandar').html("");
 	var element = document.getElementById('caleandar');
@@ -83,3 +100,4 @@ function renderCalendar() {
 
 renderCalendar();
 renderDate();
+refreshAt(0, 0, 0);
