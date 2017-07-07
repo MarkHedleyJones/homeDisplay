a9c6e4876644b2a759ac85ce4bd2d9997e3c4dbb
Mark H. Jones <markjones112358@gmail.com>
2017-07-08 10:13:47 +1200

    Fixed missing calendar after previous commit

--- http/js/main.js
+++ http/js/main.js
@@ -289,19 +289,19 @@ function generateCalendar(event_list) {
     out += '</div>';
     if (i % 7 == 6) out += '</div>';
     startdate.setDate(startdate.getDate() + 1);
-
-    out = '';
-    out += today.getFullYear()
-    out += '-';
-    if ((today.getMonth() + 1) < 10) out += '0';
-    out += today.getMonth() + 1;
-    out += '-';
-    if (today.getDate()) out += '0';
-    out += today.getDate();
-    $("#date-iso").html(out);
   }
 
   master.html(out);
+
+  out = '';
+  out += today.getFullYear()
+  out += '-';
+  if ((today.getMonth() + 1) < 10) out += '0';
+  out += today.getMonth() + 1;
+  out += '-';
+  if (today.getDate()) out += '0';
+  out += today.getDate();
+  $("#date-iso").html(out);
 }
 
 getCalendar();
